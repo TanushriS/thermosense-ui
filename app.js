7b4f3bd97f62c05f8f31aaef14e6bb537ea6f926
Tanushri Sukhwal <139938767+TanushriS@users.noreply.github.com>
2025-07-26 03:29:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -693,7 +693,7 @@ const ChartDisplay = ({ isVisible }) => {
         options: {
           responsive: true,
           maintainAspectRatio: false,
-          animation: { duration: 70 },
+          animation: { duration: 5 },
           plugins: { legend: { display: false } },
           scales: {
             y: {
