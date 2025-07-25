2f3f958c7b9740f7d6ac708fa64e583acbf88916
Tanushri Sukhwal <139938767+TanushriS@users.noreply.github.com>
2025-07-26 03:40:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -763,7 +763,7 @@ const ChartDisplay = ({ isVisible }) => {
 
         return newData;
       });
-    }, 10);
+    }, 1000);
 
     return () => clearInterval(interval);
   }, [isPaused, deviceTemp, weatherData, batteryData]);
